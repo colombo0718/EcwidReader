7f35c486ae065853fe514870d19ca5b977d05b1b
colombo0718 <colombo0718@gmail.com>
2017-05-24 15:55:31 +0800

    list ids

--- reader.js
+++ reader.js
@@ -1,11 +1,29 @@
 const fs=require('fs');
 
-var data0=fs.readFileSync('orders_2017-05-22_13-38.csv','utf-8')
+var data0=fs.readFileSync('orders_2017-05-22_13-38.csv','utf-8'),
+    data1='',
+    output='';
 
-for(var i=0;i<=7269;i++){
-	if(data0.split('\n')[i].split(';').length>1){
-		console.log(data0.split('\n')[i])
+var length=data0.split('\n').length
+for(var i=0;i<length;i++){
+	var head=data0.split('\n')[i].split(';')[0],
+	    line=data0.split('\n')[i];
+	if(head>0){
+		data1+='\n'+line+','
+	}else{
+		data1+=line+','
+	}
+}
+fs.writeFile('data1.txt',data1)
+
+length=data1.split('\n').length
+console.log(length)
+for(var i=0;i<length;i++){
+	var id=data1.split('\n')[i].split(';')[0]
+	if(id>574){
+		output+=id+'\n'
 	}
 }
 
-//console.log(aa.split('\n').length)
+//fs.writeFile('temp.txt','')
+fs.writeFile('temp.txt',output)
