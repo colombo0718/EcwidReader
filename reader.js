4af20fbf1d4f0e062696908b5784af12fe0c1b41
colombo0718 <colombo0718@gmail.com>
2017-05-25 16:32:09 +0800

    almost finish

--- reader.js
+++ reader.js
@@ -1,13 +1,13 @@
 const fs=require('fs');
 
 var data0=fs.readFileSync('orders_2017-05-22_13-38.csv','utf-8'),
-    data1='',
-    output='';
+	data1='',
+	output='';
 
 var length=data0.split('\n').length
 for(var i=0;i<length;i++){
 	var head=data0.split('\n')[i].split(';')[0],
-	    line=data0.split('\n')[i];
+		line=data0.split('\n')[i];
 	if(head>0){
 		data1+='\n'+line+','
 	}else{
@@ -19,9 +19,26 @@ fs.writeFile('data1.txt',data1)
 length=data1.split('\n').length
 console.log(length)
 for(var i=0;i<length;i++){
-	var id=data1.split('\n')[i].split(';')[0]
+	var line=data1.split('\n')[i],
+		id=data1.split('\n')[i].split(';')[0];
 	if(id>574){
-		output+=id+'\n'
+		var clas=line.split(';')[3],
+			name=line.split('學員姓名:')[1].split(',')[0],
+			scho=line.split('就讀學校:')[1].split(',')[0],
+			grad=line.split('就讀年級:')[1].split(',')[0],
+			pare=line.split('家長姓名:')[1].split(',')[0],
+			phon=line.split('聯絡電話:')[1].split(',')[0],
+			mail=line.split(';')[1],
+			eche=line.split('報名梯次:')[1].split(',')[0],
+			offe=line.split('優惠條件:')[1].split(',')[0],
+			mone=line.split(';')[5],
+			comp=line.split('電腦需求:')[1].split(',')[0],
+			aids=line.split('教具選購:').length;
+			if(comp[0]=='自'){comp=''}
+			if(aids>1){aids=line.split('教具選購:')[1].split(',')[0]}
+				else{aids=''}
+			if(aids.length>20){aids=line.split('教具選購:')[1].split('";')[0]}
+		output=output+id+','+clas+','+name+','+scho+','+grad+','+pare+','+phon+','+mail+','+eche+','+offe+','+mone+','+comp+','+aids+'\n'
 	}
 }
 
